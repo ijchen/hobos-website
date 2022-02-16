bd19a091c31f6c02cb242674a80d5f2a4653d12e
Isaac Chen <isaacjchen1@gmail.com>
2022-02-16 18:07:14 -0500

    Updated roster page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-// Nothing for now!
+// Ensure all images have alt tags
+$(() => {
+	setTimeout(() => {
+		$("img").each((i, elem) => {
+			if(elem.alt === "") {
+				console.warn("<img> tag doesn't have alt text!", elem);
+			}
+		})
+	}, 1000);
+});
--- scripts/roster.js
+++ scripts/roster.js
@@ -53,8 +53,16 @@ function createCardFromData(playerData) {
 	cardBgCont.classList.add("cardBackgroundContainer");
 	let cardBgElem = document.createElement("img");
 	cardBgElem.src = getHeroImageSource(playerData.mains[0]);
-	cardBgElem.alt = "TODO";
+	cardBgElem.alt = `The hero ${playerData.mains[0]} from the video game Overwatch`;
 	cardBgElem.classList.add("cardBackground");
+	// If they attempt to open the context menu (likely to download) on
+	// copyrighted images, show a warning about that and cancel the context menu
+	// https://www.blizzard.com/en-us/legal/c1ae32ac-7ff9-4ac3-a03b-fc04b8697010/blizzard-legal-faq#1649365241
+	// https://www.blizzard.com/en-us/legal/9c9cb70b-d1ed-4e17-998a-16c6df46be7b/copyright-notices
+	$(cardBgElem).on("contextmenu", function(e) {
+		alert("These images are copyrighted by Blizzard Entertainment.\n\n®2016 Blizzard Entertainment, Inc. All rights reserved. Overwatch is a trademark or registered trademark of Blizzard Entertainment, Inc. in the U.S. and/or other countries.");
+		return false; // Cancel showing the context menu
+	});
 	cardBgCont.appendChild(cardBgElem);
 	cardElem.append(cardBgCont);
 	
@@ -101,6 +109,33 @@ function createCardFromData(playerData) {
 function processPlayerData(allPlayerData) {
 	for(let playerData of allPlayerData.players) {
 		createCardFromData(playerData);
+	}
+
+	// It is a requirement that I use at least one of these... I am aware this
+	// is not the "correct" way to do this. Trust me this code would not be
+	// disgusting if it weren't for a grade
+	$(".card").hide();
+	const animateNext = (() => {
+		// basically made a rust iterator because my assignment is weird
+		let idx = 0;
+		let cards = $(".card").get();
+		
+		const f = () => {
+			if(idx < cards.length) {
+				// $(cards[idx]).fadeIn(500, f);
+				$(cards[idx]).slideDown(500, f);
+				
+				idx++;
+			}
+		}
+		
+		return f;
+	})();
+	let cards = $(".card");
+	for(let i = 0; i < cards.length; i++) {
+		$(cards[i]).fadeIn(1000 + 500 * i, () => {
+			console.log(cards[i]);
+		});
 	}
 }
 
